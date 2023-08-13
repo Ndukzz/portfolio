43e9357225da496053e571f78a8d72c7a408e52f
Chukwuebuka Ndukwe <ndukz@Chukwuebukas-MacBook-Pro.local>
2023-08-13 17:41:36 -0500

    set the skills section

--- src/components/Skills/SkillItems.jsx
+++ src/components/Skills/SkillItems.jsx
@@ -2,20 +2,62 @@ import React from 'react'
 import classes from "./SkillItems.module.css"
 
 const SkillItems = (props) => {
+
+  const values = Object.values(props.data)
+  console.log(values);
+
   return (
     <div className={classes.skillContainer}>
       <div className={classes.skillIcons}>
         {/* THIS IS WHERE ALL THE FLOATING ICONS WILL GO */}
       </div>
-      <div className={classes.skillModules}>
+
+      <div className={classes.skillModules}>   
+{/* THIS IS THE LANGUAGES SECTION  */}
         <section className={classes.skillModule}>
           <h4>Languages</h4>
-          {props.data.languages.map((language) => {
-            return language.name
-          })}
+          <div> 
+            {props.data.languages.map((language) => {
+              return (language.name + " ")
+            })}
+          </div>
         </section>
-        <section className="skillModule"></section>
-        <section className="skillModule"></section>
+{/* THIS IS THE DATABASES SECTION */}
+        <section className={classes.skillModule}>
+          <h4>Languages</h4>
+          <div> 
+            {props.data.languages.map((language) => {
+              return (language.name + " ")
+            })}
+          </div>
+        </section>  
+{/* THIS IS THE TOOLS SECTION */}
+        <section className={classes.skillModule}>
+          <h4>Languages</h4>
+          <div> 
+            {props.data.languages.map((language) => {
+              return (language.name + " ")
+            })}
+          </div>
+        </section>
+{/* THIS IS THE FRAMEWORKS SECTION */}
+        <section className={classes.skillModule}>
+          <h4>Languages</h4>
+          <div> 
+            {props.data.languages.map((language) => {
+              return (language.name + " ")
+            })}
+          </div>
+        </section>
+{/* THIS IS THE TOOLS SECTION */}
+        <section className={classes.skillModule}>
+          <h4>Languages</h4>
+          <div> 
+            {props.data.languages.map((language) => {
+              return (language.name + " ")
+            })}
+          </div>
+        </section>
       </div>
     </div>
   )
